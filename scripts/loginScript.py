a1d36060036f6a9520dde60c0ac51111b101b0a5
Julia Navarro <79175907+JuliaVNavarro@users.noreply.github.com>
2023-04-12 11:39:57 -0700

    updated to current chrome commands

--- scripts/loginScript.py
+++ scripts/loginScript.py
@@ -1,13 +1,12 @@
 import os
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 # Set the path to the directory where new .csv files will be created
-watch_directory = '/Users/karypaquot/Documents/GitHub/Smishing-Project/scripts'
+watch_directory = 'C:/Users/brave/Desktop/Smishing/Smishing-Project-Karina-s-Branch/scripts'
 
 # Set the URL of the login page
 login_url = 'https://www.facebook.com'
@@ -17,15 +16,23 @@ driver_path = '/usr/local/bin/chromedriver'
 
 # Create a function that logs in using the given username and password
 def login(username, password):
-    # Create a new instance of the Chrome browser
-    driver = webdriver.Chrome(driver_path)
+    
+    # Disable certificate verification
+    options = Options()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    options.add_argument('--ignore-certificate-errors-spki-list')
+    
+    # Initialize the driver object using the service object
+    service = Service('C:/Users/brave/Downloads/chromedriver_win32 (1)/chromedriver.exe')
+    driver = webdriver.Chrome(service=service, options=options)
 
     # Navigate to the login page
     driver.get(login_url)
 
     # Enter the username and password
-    input_email = driver.find_element_by_name('email')
-    input_pass = driver.find_element_by_name('pass')
+    input_email = driver.find_element(By.NAME, 'email')
+    input_pass = driver.find_element(By.NAME, 'pass')
 
     input_email.clear()
     input_email.send_keys(username)
@@ -37,52 +44,77 @@ def login(username, password):
 
     # Wait for the page to load
     time.sleep(5)
+
     # Set the url to the settings page
     settings_url = 'https://www.facebook.com/settings/?tab=account'
+
     # navigate to the settings page
     driver.get(settings_url)
+
     # Wait for the page to load
     time.sleep(5)
+
     #set the url to the security page 
     security_url = 'https://www.facebook.com/settings?tab=security'
+
     #navigate to the security page 
     driver.get(security_url)
+
     # Wait for the page to load
     time.sleep(5)
 
-    #iframe = driver.find_elements_by_tag_name('iframe')[0]
-    #driver.switch_to.frame(iframe)
+    # Locate the first iframe where the HTML doc is
+    iframes = driver.find_elements(By.TAG_NAME, 'iframe')
+    iframe = iframes[0]
 
-    # switch to parent frame
-    driver.switch_to.default_content()
-
-    # switch to iframe
-    iframe = driver.find_element_by_css_selector("iframe.x76ihet[src='https://www.facebook.com/settings?tab=security']")
+    # Switch to that frame
     driver.switch_to.frame(iframe)
 
-    # click on the button
-    edit_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Edit']")))
-    edit_button.click()
-    # Enter the old password and new password 
-    #input_password_old = driver.find_element_by_id('password_old')
-    #input_password_old.clear()
-    #input_password_old.send_keys(password)
 
-    #input_new_password = driver.find_element_by_id('password_new')
-    #input_new_password.clear()
-    #input_new_password.send_keys(password)
+    # locate the login div class inside the iframe
+    login_class = driver.find_element(By.CSS_SELECTOR, 'div._1xpm._4-u2._4-u8')
 
-    #input_retype_new_password = driver.find_element_by_id('password_confirm')
-    #input_retype_new_password.clear()
-    #input_retype_new_password.send_keys(password)
+    # locate the inner div class in the login div class
+    inner_div = login_class.find_element(By.CSS_SELECTOR, 'div._1nfz._4-u3')
 
+    # locate the change password table that is clickable
+    clickable_table = inner_div.find_element(By.CSS_SELECTOR, 'table._4p8y.uiGrid._51mz')
+
+    # click the table to make it viewable
+    clickable_table.click()
+
+    time.sleep(3)
+    
+    # locate the password old input field 
+    pass_old_id = driver.find_element(By.ID, 'password_old')
+
+    # send the password to the input field
+    pass_old_id.send_keys(password)
+
+    # get the new password field by id 
+    input_new_password = driver.find_element(By.ID, 'password_new')
+
+    # set the new password
+    new_password = 'WeLove378!'
+    input_new_password.send_keys(new_password)
+
+    # get the retype new password field bye id
+    input_retype_new_password = driver.find_element(By.ID, 'password_confirm')
+    
+    # set the retyped password
+    input_retype_new_password.send_keys(new_password)
+
+    # uncomment this to save the new password 
     #save_changes = driver.find_element_by_id('u_b_0_WN')
     #save_changes.click()
-
-    #button for the edit class="_1nf- _4jy0 _4jy3 _517h _51sy _42ft"
-
+    
+    # Keep the window open
+    input('Press any key to exit...')
+    
     # Close the browser
-    #driver.quit()
+    driver.quit()
+    
+    
 
 # Set the initial list of files in the directory
 before = dict ([(f, None) for f in os.listdir(watch_directory)])
