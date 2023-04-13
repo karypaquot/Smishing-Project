780a3c8e6cabf569ad4483f80a92ee9cca3d73cb
Karina Hernandez <Karina.Hernandez08@student.csulb.edu>
2023-04-12 22:32:47 -0700

    copied login script
    copied the contents of login script 1 to login script 2 to match the methods that work with Julias web driver.

--- scripts/loginScript.py
+++ scripts/loginScript.py
@@ -1,11 +1,11 @@
 import os
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+#from selenium.common.exceptions import NoSuchElementException
 
 
 # Set the path to the directory where new .csv files will be created
