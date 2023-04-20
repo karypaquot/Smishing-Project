4d49f79b7e7ec73d97740408d69eecb4c4d95c34
Karina Hernandez <Karina.Hernandez08@student.csulb.edu>
2023-04-20 10:16:45 -0700

    Update subscriber_script.py

--- texting/subscriber_script.py
+++ texting/subscriber_script.py
@@ -1,5 +1,6 @@
 import requests
 
+# this is the End point URL for the API to make the call
 url = 'https://api.mobile-text-alerts.com/v3/subscribers/bulk'
 
 headers = {
@@ -7,6 +8,7 @@ headers = {
     'Authorization': 'Bearer 14e5aa70fb86e2ccedf4d289c5e940'
 }
 
+# this creates a data set with the subscibers info
 data = [
     {
         "firstName": "Karina",
@@ -19,6 +21,7 @@ data = [
     }
 ]
 
+# Sends a post request to the URL end point with the API to connect
 response = requests.post(url, headers=headers, json=data)
 
 print(response.text)
